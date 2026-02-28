0ecec9c27b05022ef33946bdc0ff4e692f5b4c26
DK <na@gmail.com>
2026-02-27 16:52:54 -0800

    oh yea

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,17 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-9MB3P84ZVL" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-9MB3P84ZVL');
+            `,
+          }}
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link
